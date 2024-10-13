42196571aacc7aeb59a0cca44f0b615e3b10d29d
souhair salah <sou6sal@gmail.com>
2024-10-13 18:57:31 +0300

    new data

--- src/components/EmpListing/index.tsx
+++ src/components/EmpListing/index.tsx
@@ -4,19 +4,25 @@ import { MdModeEdit } from "react-icons/md";
 import { FaTrashAlt } from "react-icons/fa";
 
 
-type employee = {
+type Address = {
+  address: string,
+  city: string,
+  state: string,
+}
+type users = {
   id : number ;
-  name : string;
+  firstName : string;
   email : string;
   phone : number;
+  address : Address;
 }
 const EmpListing = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:8002/employees")
+    fetch("https://dummyjson.com/users")
       .then((responsive) => responsive.json())
-      .then((data) => setData(data))
+      .then((data) => setData(data.users))
       .catch((err) => {
         console.log("errorrrrrr");
       });
@@ -37,16 +43,18 @@ const EmpListing = () => {
                 <th className="px-10 border border-slate-300">NAME</th>
                 <th className="px-10 border border-slate-300">EMAIL</th>
                 <th className="px-10 border border-slate-300">PHONE</th>
+                <th className="px-10 border border-slate-300">ADDRESS</th>
                 <th className="px-10 border border-slate-300">ACTION</th>
               </tr>
             </thead>
             <tbody>
-              {data.map((data:employee) => (
+              {data.map((data:users) => (
                 <tr>
                   <td className="p-1 border border-slate-300">{data.id}</td>
-                  <td className="p-1 text-start border border-slate-300">{data.name}</td>
+                  <td className="p-1 text-start border border-slate-300">{data.firstName}</td>
                   <td className="p-1 text-start border border-slate-300">{data.email}</td>
                   <td className="p-1 text-start border border-slate-300">{data.phone}</td>
+                  <td className="p-1 text-start border border-slate-300">{data.address?.city|| "Not Available"}</td>
                   <td className=" space-x-5 text-center border border-slate-300">
                     <button className=""><MdModeEdit /></button>
                     <button className="text-red-600"><FaTrashAlt /></button>
