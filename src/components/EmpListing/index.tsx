a36cbcc7588fcb79f6a913a49015a452164dfa02
souhair salah <sou6sal@gmail.com>
2024-11-04 17:19:17 +0200

    skelton and menu

--- src/components/EmpListing/index.tsx
+++ src/components/EmpListing/index.tsx
@@ -5,6 +5,8 @@ import { MdModeEdit } from "react-icons/md";
 import { FaTrashAlt } from "react-icons/fa";
 import Image from "next/image";
 import Pagination from "../Pagination";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 type Company = {
   name: string;
@@ -19,6 +21,18 @@ type users = {
   company: Company;
 };
 const EmpListing = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+  const handlePageChange = (page: number) => setCurrentPage(page);
+
+  const [activeId, setActiveId] = useState<number | null>(null); // حالة لتخزين id العنصر المحدد
+
+  const toggleOptionsMenu = (id: number) => {
+    setActiveId((prevActiveId) => (prevActiveId === id ? null : id));
+    // setIsMenuVisible(!isMenuVisible); // تغيير حالة العنصر أو إخفاؤه إذا كان نفس id
+  };
+
   // start pagination
   const [data, setData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
@@ -33,60 +47,59 @@ const EmpListing = () => {
     currentPage * rowsPerPage
   );
 
-  useEffect(() => {
-    fetch("https://dummyjson.com/users")
-      .then((res) => res.json())
-      .then((data) => setData(data.users))
-      .catch((error) => console.error("Failed to fetch users:", error));
-  }, []);
-
-  const handlePageChange = (page: number) => setCurrentPage(page);
-
-  // const rowsPerPage = 6;
-  // const [data, setData] = useState([]);
-  // const [currentData, setCurrentData] = useState([]);
-  // const [currentPage, setCurrentPage] = useState(1);
-  // const endIndex = currentPage * rowsPerPage;
-  // const startIndex = endIndex - rowsPerPage;
-  // const totalPages = data.length / rowsPerPage;
-  // const [num, setNum] = useState(1);
-
   // useEffect(() => {
   //   fetch("https://dummyjson.com/users")
-  //     .then((responsive) => responsive.json())
-  //     .then((data) => {
-  //       const users = data.users;
-  //       setData(users);
-  //       if (users.length > 0) {
-  //         setCurrentData(users.slice(startIndex, endIndex));
-  //       }
-  //     })
-  //     .catch((err) => {
-  //       console.log("errorrrrrr");
-  //     });
-  // }, [currentPage, num]);
+  //     .then((res) => res.json())
+  //     .then((data) => setData(data.users))
+  //     .catch((error) => console.error("Failed to fetch users:", error));
+  // }, []);
 
-  // const GoPrevious = (): void => {
-  //   if (currentPage > 1) {
-  //     setCurrentPage((prev) => prev - 1);
-  //     setNum((prev) => (prev > 1 ? prev - 1 : prev));
-  //   }
-  // };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://dummyjson.com/users");
+        if (!response.ok) {
+          throw new Error("Failed to fetch users.");
+        }
+        const result = await response.json();
+        setData(result.users);
+      } catch (error) {
+        console.error("Failed to fetch users:", error);
+        // setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
-  // const GoNext = (): void => {
-  //   if (currentPage < totalPages) {
-  //     setCurrentPage(currentPage + 1);
-  //   }
-  //   if (num < totalPages - 2) {
-  //     setNum(num + 1);
-  //   }
+    fetchData();
+  }, []);
 
-  // };
-  // const handleCurrentPage = (value: number): void => {
-  //   if (value >= 1 && value <= totalPages) {
-  //     setCurrentPage(value);
-  //   }
-  // };
+  if (error) {
+    return <div>{error}</div>;
+}
+
+  const renderSkeleton = () =>
+    Array(rowsPerPage)
+      .fill(0)
+      .map((_, index) => (
+        <tr key={index}>
+          <td className="p-4">
+            <Skeleton width={300} />
+          </td>
+          <td className="p-4">
+            <Skeleton width={200} />
+          </td>
+          <td className="p-4">
+            <Skeleton width={50} />
+          </td>
+          <td className="p-4">
+            <Skeleton width={200} />
+          </td>
+          <td className="p-4">
+            <Skeleton width={50} />
+          </td>
+        </tr>
+      ));
 
   return (
     <div className="container">
@@ -120,62 +133,66 @@ const EmpListing = () => {
               </tr>
             </thead>
             <tbody className="">
-              {currentData.map((data: users) => (
-                <tr key={data.id} className="odd:bg-white even:bg-gray-50">
-                  {/* <td className="p-1 border-b border-slate-300">{data.id}</td> */}
-                  <td className=" p-3 text-start border-b border-slate-300">
-                    <div className="flex  gap-2 items-center">
-                      <div>
-                        <Image
-                          src={data.image}
-                          alt="user image"
-                          width={30}
-                          height={30}
-                        />
-                      </div>
-                      <div className=" flex flex-col">
-                        <span className="text-gray-900 font-medium">
-                          {data.firstName}
-                        </span>
-                        <span className="text-sm">{data.email}</span>
-                      </div>
-                    </div>
-                  </td>
-
-                  {/* <td className="p-1 text-start border border-slate-300">
-                    {data.email}
-                  </td> */}
-                  <td className="p-3 text-start border-b border-slate-300">
-                    {data.phone}
-                  </td>
-                  <td className="p-3 border-b border-slate-300 ">
-                    {/* "p-1 border border-[#dc2626] bg-[#fee2e2] rounded-full w-24 h-8" */}
-                    <div
-                      className={`inline-block items-center px-1   border rounded-full font-medium  ${
-                        data.role == "admin"
-                          ? ` border-[#dc2626] bg-[#fee2e2] text-[#dc2626]  `
-                          : data.role == "moderator"
-                          ? ` border-[#047857] bg-[#a7f3d0] text-[#047857]`
-                          : ` border-[#1d4ed8] bg-[#bfdbfe] text-[#1d4ed8]`
-                      }`}
+              {isLoading
+                ? renderSkeleton()
+                : currentData.map((data: users) => (
+                    <tr
+                      key={data.id}
+                      className="odd:bg-white even:bg-gray-50 cursor-pointer"
                     >
-                      {data.role}
-                    </div>
-                  </td>
-                  <td className="p-3 text-start border-b border-slate-300">
-                    {data.company?.name || "Not Available"}
-                  </td>
-                  <td className="p-3 space-x-5 text-center border-b border-slate-300">
-                    {/* <button className="">
+                      {/* <td className="p-1 border-b border-slate-300">{data.id}</td> */}
+                      <td className=" p-3 text-start border-b border-slate-300">
+                        <div className="flex  gap-2 items-center">
+                          <div>
+                            <Image
+                              src={data.image}
+                              alt="user image"
+                              width={30}
+                              height={30}
+                            />
+                          </div>
+                          <div className=" flex flex-col">
+                            <span className="text-gray-900 font-medium">
+                              {data.firstName}
+                            </span>
+                            <span className="text-sm">{data.email}</span>
+                          </div>
+                        </div>
+                      </td>
+                      <td className="p-3 text-start border-b border-slate-300">
+                        {data.phone}
+                      </td>
+                      <td className="p-3 border-b border-slate-300 ">
+                        {/* "p-1 border border-[#dc2626] bg-[#fee2e2] rounded-full w-24 h-8" */}
+                        <div>{data.role}</div>
+                      </td>
+                      <td className="p-3 text-start border-b border-slate-300">
+                        {data.company?.name || "Not Available"}
+
+                        <div></div>
+                      </td>
+                      <td className="p-3 space-x-5 text-center border-b border-slate-300">
+                        {/* <button className="">
                       <MdModeEdit />
                     </button>
                     <button className="text-red-600">
                       <FaTrashAlt />
                     </button> */}
-                    <button className="">...</button>
-                  </td>
-                </tr>
-              ))}
+                        <button onClick={() => toggleOptionsMenu(data.id)}>
+                          ...
+                        </button>
+                        <div
+                          className={`grid grid-cols-1 border-[1px]  rounded ${
+                            activeId === data.id ? "block" : "hidden"
+                          }
+                      }`}
+                        >
+                          <span className="border-b ">delete</span>
+                          <span>edit</span>
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
             </tbody>
           </table>
           <div>
@@ -187,11 +204,85 @@ const EmpListing = () => {
           </div>
         </div>
       </div>
+      {error && <div className="text-red-500">{error}</div>}
     </div>
   );
 };
 
 export default EmpListing;
+
+// className={`inline-block items-center px-1   border rounded-full font-medium  ${
+//   data.role == "admin"
+//     ? ` border-[#dc2626] bg-[#fee2e2] text-[#dc2626]  `
+//     : data.role == "moderator"
+//     ? ` border-[#047857] bg-[#a7f3d0] text-[#047857]`
+//     : ` border-[#1d4ed8] bg-[#bfdbfe] text-[#1d4ed8]`
+// }`}
+
+// return (
+//   <table className="w-full text-left border">
+//     <thead>
+//       <tr>
+//         <th className="p-3 font-semibold border-b border-slate-300 text-start">NAME</th>
+//         <th className="p-3 font-semibold border-b border-slate-300 text-start">PHONE</th>
+//         <th className="p-3 font-semibold border-b border-slate-300 text-start">ROLE</th>
+//         <th className="p-3 font-semibold border-b border-slate-300 text-start">COMPANY</th>
+//         <th className="p-3 font-semibold border-b border-slate-300 text-start">Menu</th>
+//       </tr>
+//     </thead>
+//     <tbody>
+//       {Array(5).fill(0).map((_, index) => (
+//         <SkeletonRow key={index} />
+//       ))}
+//     </tbody>
+//   </table>
+// );
+
+// const rowsPerPage = 6;
+// const [data, setData] = useState([]);
+// const [currentData, setCurrentData] = useState([]);
+// const [currentPage, setCurrentPage] = useState(1);
+// const endIndex = currentPage * rowsPerPage;
+// const startIndex = endIndex - rowsPerPage;
+// const totalPages = data.length / rowsPerPage;
+// const [num, setNum] = useState(1);
+
+// useEffect(() => {
+//   fetch("https://dummyjson.com/users")
+//     .then((responsive) => responsive.json())
+//     .then((data) => {
+//       const users = data.users;
+//       setData(users);
+//       if (users.length > 0) {
+//         setCurrentData(users.slice(startIndex, endIndex));
+//       }
+//     })
+//     .catch((err) => {
+//       console.log("errorrrrrr");
+//     });
+// }, [currentPage, num]);
+
+// const GoPrevious = (): void => {
+//   if (currentPage > 1) {
+//     setCurrentPage((prev) => prev - 1);
+//     setNum((prev) => (prev > 1 ? prev - 1 : prev));
+//   }
+// };
+
+// const GoNext = (): void => {
+//   if (currentPage < totalPages) {
+//     setCurrentPage(currentPage + 1);
+//   }
+//   if (num < totalPages - 2) {
+//     setNum(num + 1);
+//   }
+
+// };
+// const handleCurrentPage = (value: number): void => {
+//   if (value >= 1 && value <= totalPages) {
+//     setCurrentPage(value);
+//   }
+// };
 
 // const GoPrevious = (): void => {
 //   if (currentPage > 1) {
